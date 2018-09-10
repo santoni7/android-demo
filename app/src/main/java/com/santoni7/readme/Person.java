f7e2023db6fad8de2bafd6234baf949df3c05629
Anton Sakhniuk <santoni7win8@gmail.com>
2018-09-10 16:05:25 +0300

    Init commit: Main screen with RecyclerView

---
+++ app/src/main/java/com/santoni7/readme/Person.java
@@ -0,0 +1,57 @@
+package com.santoni7.readme;
+
+public class Person {
+    private String id;
+    private String firstName;
+    private String secondName;
+    private String avatarUrl;
+    private int age;
+
+    public Person(String id, String firstName, String secondName, String avatarUrl, int age){
+        this.id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.avatarUrl = avatarUrl;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
